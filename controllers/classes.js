d7bc698448a3785be0dd351306b13136732af469
Niall <niall.grant1992@gmail.com>
2017-08-24 18:07:37 +0100

    Working on fullEnroll method which will have to check if user is enrolled, not a trainer or if class is full

--- controllers/classes.js
+++ controllers/classes.js
@@ -25,17 +25,47 @@ const classes = {
 
   fullEnroll(request, response) {
     let user = accounts.getCurrentUser(request);
-    let memberArr = classStore.getClassList(request.params.classid)[0].memberList;
+    let userid = user.id;
+    let classArr = classStore.getClassList(request.params.classid)[0];
+    let lessonList = classArr.lessonList;
 
-    if (memberArr.indexOf(user.id) >= 0 || user.trainer) {
-      logger.info('Member already enrolled or is a trainer: ', memberArr);
-      response.redirect('/classes');
+    //To check if class isn't full or user isn't a trainer
+    if (classArr.maxMembers - classArr.currMembers !== 0 || !user.trainer) {
+      let memberCount = 0;
+
+      for (let i = 0; i < lessonList.length; i++) {
+        for (let k = 0; k < lessonList[i].memberList.length; k++) {
+          if (memberList[k].userid === user.id) {
+            logger.info('Member already enrolled');
+            response.redirect('/classes');
+          }
+        }
+      }
+
+      //a for each loop to go through the individual lessons and add the user to each lesson
+      for (let i = 0; i < lessonList.length; i++) {
+        let memberList = lessonList[i].memberList;
+
+        //Checks to make sure user's id isn't already stored for that particular lesson
+        if (memberList.indexOf(userid) > 0) {
+          memberList.push(userid);
+          logger.info('Member added to lesson: ', lessonList.lessonid);
+        }
+
+        //This is to help prevent potential bugs if a member signs up for a certain class and chooses to fully enroll later
+        else {
+          memberCount++;
+          logger.info('Member already in lesson: ', lessonList.lessonid);
+        }
+      }
+
+      logger.info('Enrolling: ', user.trainer);
     }
     else {
-      memberArr.push(user.id);
-      logger.info('Enrolling: ', memberArr);
-      response.redirect('/classes');
+      logger.info('Class is full or user is a trainer: ');
     }
+
+    response.redirect('/classes');
   },
 };
 
