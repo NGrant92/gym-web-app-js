cf6c8ed1db812ef410e7b236a2ac5476e61c3798
Niall <niall.grant1992@gmail.com>
2017-08-25 01:43:26 +0100

    Rearranged the display of individual classes. Restyled the classes.hbs layout. Added new functions to handlebarregister helper to help alter the appearance of enrolling buttons. Replace currMembers value with bio in class-store.json

--- utils/handlebarsRegisterHelper.js
+++ utils/handlebarsRegisterHelper.js
@@ -7,4 +7,35 @@ Handlebars.registerHelper('toLowerCase', function (str) {
   return str.toLowerCase();
 });
 
+Handlebars.registerHelper('checkAllLessons', function (classObj) {
+  let lessonList = classObj.lessonList;
+  let isFull = true;
+
+  for (let i = 0; i < lessonList.length; i++) {
+    if (lessonList[i].memberList.length < classObj.maxMembers) {
+      isFull = false;
+      break;
+    }
+  }
+
+  return isFull;
+});
+
+Handlebars.registerHelper('checkLesson', function (memberList, userid) {
+  return memberList.indexOf(userid);
+});
+
+Handlebars.registerHelper('checkAllMemLessons', function (lessonList, userid) {
+  let isEnrolled;
+
+  for (let i = 0; i < lessonList.length; i++) {
+    if (lessonList[i].memberList.indexOf(userid) >= 0) {
+      isEnrolled = true;
+      break;
+    }
+  }
+  return isEnrolled;
+});
+
+
 module.exports = Handlebars;
